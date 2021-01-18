d6a57580ef501d00ff0b59c54b6610ea964147dc
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2021-01-18 20:35:36 +0530

    added local time

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ let humdity = document.querySelector("#value-humidity");
 let co2 = document.querySelector("#value-co2");
 let ammonia = document.querySelector("#value-ammonia");
 let airpressure = document.querySelector("#value-airpressure");
-let airquality = document.querySelector("#value-airquality")
+let airquality = document.querySelector("#value-airquality");
+let date = document.querySelector("#date-value");
+let time = document.querySelector("#time-value");
+
+var today = new Date();
+date.textContent = today.toDateString();
+time.textContent = today.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});;
+
 
 fetch(api)
     .then((response) => {
